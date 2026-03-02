b6f231a0c286cf529b65758e10834c98f4d99b88
hemanthkumarmajji <hemanthkumar3628@gmail.com>
2026-03-02 13:18:31 +0530

    Update OOPSBannerApp.java

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,21 +1,18 @@
-/**
- * OOPSBannerApp
- *
- * UC2: Render OOPS as Banner using Print Statements
- *
- * @author Hemanth
- * @version 2.0
- */
 public class OOPSBannerApp {
 
     public static void main(String[] args) {
+        String[] oopsBanner = {
+            String.join("", "  *** ", "   *** ", " ***** ", " ***** "),
+            String.join("", " ** ** ", " **  ** ", " ** ** ", " ** "),
+            String.join("", "**   ** ", "**  ** ", " ** ** ", " ** "),
+            String.join("", "**   ** ", "**  ** ", " ***** ", " *** "),
+            String.join("", "**   ** ", "**  ** ", " ** ", "   ** "),
+            String.join("", " ** ** ", " ** ** ", " ** ", "   ** "),
+            String.join("", "  *** ", "   *** ", " ** ", " ***** ")
+        };
 
-        System.out.println("  ***     " + "  ***     " + " *****   " + " ***** ");
-        System.out.println(" ** **    " + " ** **    " + " **  **  " + " **    ");
-        System.out.println("**   **   " + "**   **   " + " **   ** " + " **    ");
-        System.out.println("**   **   " + "**   **   " + " *****   " + " ***   ");
-        System.out.println("**   **   " + "**   **   " + " **      " + "   **  ");
-        System.out.println(" ** **    " + " ** **    " + " **      " + "   **  ");
-        System.out.println("  ***     " + "  ***     " + " **      " + " ***** ");
+        for (String line : oopsBanner) {
+            System.out.println(line);
+        }
     }
 }
